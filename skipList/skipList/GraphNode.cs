8088f36a183ec2a2b02a64ddb608dca047e29fa2
L-taly <litanlinlitanlin@outlook.com>
2017-03-15 14:51:18 +0800

    GraphNode

---
+++ skipList/skipList/GraphNode.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace skiplist1
+{
+    class GraphNode
+    {
+        private int key;
+        private string name;
+
+        public int Key
+        {
+            set { key = value; }
+            get { return key; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public GraphNode()
+        {
+
+        }
+
+        public GraphNode(string Name)
+        {
+            this.name = Name;
+        }
+    }
+}
